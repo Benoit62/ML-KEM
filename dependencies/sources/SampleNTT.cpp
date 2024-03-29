297ffa3ce0ea5c85cc15f769e8a351c2ed909608
Hamz11 <hamza.berbache@student.junia.com>
2024-03-29 13:35:04 +0100

    pas testé

---
+++ dependencies/sources/SampleNTT.cpp
@@ -0,0 +1,27 @@
+#include "SampleNTT.hpp"
+#include <cmath>
+
+std::vector<int> SampleNTT(const std::vector<unsigned char>& B) {
+    std::vector<int> a;
+    int i = 0;
+    int j = 0;
+    int d1, d2;
+
+    while (j < 256) {
+        d1 = B[i] + 256 * (B[i + 1] % 16);
+        d2 = static_cast<int>(floor(B[i + 1] / 16.0)) + 16 * B[i + 2];
+
+        if (d1 < q) {
+            a.push_back(d1);
+            j++;
+        }
+
+        if (d2 < q && j < 256) {
+            a.push_back(d2);
+            j++;
+        }
+        i += 3;
+    }
+
+    return a;
+}
