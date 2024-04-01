4398d7a9e0f10ba0746918b32a76364d47b4faa9
Benoit62 <65405337+Benoit62@users.noreply.github.com>
2024-04-01 19:00:42 +0200

    Adding Go section to test other implementation

---
+++ other_languages/go/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	drawTree()
+}
+func drawTree() {
+	fmt.Println("   /\\   ")
+	fmt.Println("  /  \\  ")
+	fmt.Println(" /    \\ ")
+	fmt.Println("/______\\")
+
+	fmt.Println("   ||   ")
+	fmt.Println("   ||   ")
+	fmt.Println("   ||   ")
+}
