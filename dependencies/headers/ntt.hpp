8e3d21ee0eec2f6b434496224a7e7e93299fa372
Remiprotin <113690472+Remiprotin@users.noreply.github.com>
2024-04-02 14:10:44 +0200

    Update ntt.hpp

--- dependencies/headers/ntt.hpp
+++ dependencies/headers/ntt.hpp
@@ -1,28 +1,12 @@
-// ntt.hpp
-
 #ifndef NTT_HPP
 #define NTT_HPP
 
-#include <vector>
-#include "BitRev7.hpp"
+#include "../sources/ntt.cpp"
 
-// Fonction pour calculer la NTT en place
-void computeNTT(std::vector<int>& f, int q) {
-    int n = f.size();
-    int k = 1;
+void computeNTT(std::vector<int>& f, int q);
 
-    for (int len = 128; len >= 2; len /= 2) {
-        for (int start = 0; start < n; start += 2 * len) {
-            int zeta = BitRev7(k) % q;
-            k++;
+uint8_t BitRev7(uint8_t i);
 
-            for (int j = start; j < start + len; j++) {
-                int t = (1LL * zeta * f[j + len]) % q; 
-                f[j + len] = (f[j] - t + q) % q;       
-                f[j] = (f[j] + t) % q;                
-            }
-        }
-    }
-}
+void testComputeNTT();
 
 #endif // NTT_HPP
