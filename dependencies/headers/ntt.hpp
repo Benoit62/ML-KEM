4c8ae4695f17c84cf54b7276dd47e5dba79f6274
Remiprotin <113690472+Remiprotin@users.noreply.github.com>
2024-04-03 14:09:08 +0200

    Update ntt.hpp

--- dependencies/headers/ntt.hpp
+++ dependencies/headers/ntt.hpp
@@ -1,9 +1,13 @@
+// ntt.hpp
 #ifndef NTT_HPP
 #define NTT_HPP
-#include <vector>
-#include "BitRev7.hpp"
 
-void computeNTT(std::vector<int>& f, int q, int n, int* roots);
-void testComputeNTT();
+#include <array>
+#include <cstdint>
+
+constexpr std::size_t N = 256;
+constexpr uint32_t q = 3329; // 2^12 + 1
+
+std::array<uint32_t, N> ntt(const std::array<uint32_t, N>& f);
 
 #endif // NTT_HPP
