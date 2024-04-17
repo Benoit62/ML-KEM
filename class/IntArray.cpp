81f0716391dd7a11879f2ef976bf47b37c74a577
Enzo BAREA <enzo.barea@gmail.com>
2024-04-17 10:46:33 +0200

    ebauche KPKE + corrige XOF

--- class/IntArray.cpp
+++ class/IntArray.cpp
@@ -8,73 +8,45 @@
 using namespace std;
 
 class IntArray {
-
     private:
-    
         // Attributes
         vector<uint16_t> data;
         static const int MAX_SIZE = n;
 
     public:
-
         // Constructors
         IntArray();
-
         IntArray(int size);
-
         IntArray(const vector<uint16_t>& values);
-
         // Methods
         void set(uint16_t newValue, int index);
-        
         uint16_t get(int index);
-
         vector<uint16_t> get();
-
         size_t getSize();
-
         // Destructor
         ~IntArray();
-
+        // Operators
         friend ostream& operator<<(ostream& os, IntArray& intArr);
 };
 
 // Constructors
-
-IntArray::IntArray() : data(MAX_SIZE, 0){
-}
-
-IntArray::IntArray(int size) : data(size, 0) {
-}
-
+IntArray::IntArray() : data(MAX_SIZE, 0){}
+IntArray::IntArray(int size) : data(size, 0) {}
 IntArray::IntArray(const vector<uint16_t>& values) {
     if(values.size() > MAX_SIZE) { cout << "Input vector size exceeds limit."; }
     else { data=values; }
 }
 
 // Methods
-
-void IntArray::set(uint16_t newValue, int index) {
-    this->data[index] = newValue;
-}
-
-uint16_t IntArray::get(int index) {
-    return this->data[index];
-}
-
-vector<uint16_t> IntArray::get() {
-    return this->data;
-}
-
-size_t IntArray::getSize() {
-    return this->data.size();
-}
+void IntArray::set(uint16_t newValue, int index) { this->data[index] = newValue; }
+uint16_t IntArray::get(int index) { return this->data[index]; }
+vector<uint16_t> IntArray::get() { return this->data; }
+size_t IntArray::getSize() { return this->data.size(); }
 
 // Destructor
+IntArray::~IntArray() {}
 
-IntArray::~IntArray() {
-}
-
+// Operators
 ostream& operator<<(ostream& os, IntArray& intArr) {
     for(int i = 0; i < intArr.getSize(); i++) {
         os << intArr.get(i);
---
+++ class/KPKE.cpp
@@ -0,0 +1,50 @@
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include "ByteArray.hpp"
+#include "IntArray.cpp"
+#include "Crypto.cpp"
+#include "constants.hpp"
+
+using namespace std;
+
+class KPKE {
+    public:
+    static pair<ByteArray, ByteArray> KeyGen();
+    static ByteArray Encrypt(ByteArray ek_pke, ByteArray message, ByteArray r);
+    static ByteArray Decrypt(ByteArray dk_pke, ByteArray c);
+
+};
+
+pair<ByteArray, ByteArray> KPKE::KeyGen() {
+    ByteArray rho(32), sigma(32);
+    pair<ByteArray, ByteArray> tmp  = Crypto::G(Crypto::generateRandomBytes(32));
+    rho = tmp.first; sigma = tmp.second;
+    uint16_t N=0;
+    // TODO declare matrix here
+    for(int i=0;i<k;i++) {
+        for(int j=0;j<k;j++) {
+            // fill A_hat matrix
+        }
+    }
+}
+
+ByteArray KPKE::Encrypt(ByteArray ek_pke, ByteArray message, ByteArray r) {
+    uint16_t N=0;
+    ByteArray e = ek_pke.split(0,384*k);
+    IntArray t_hat = ByteArray::byteDecode(e, 12);
+    ByteArray rho = ek_pke.split(384*k,384*k+32);
+    // declare matrix here
+    for(int i=0;i<k;i++) {
+        for(int j=0;j<k;j++) {
+            // fill A_hat matrix
+        }
+    }
+}
+
+ByteArray Decrypt(ByteArray dk_pke, ByteArray c) {
+    ByteArray c1 = c.split(0,32*du*k);
+    ByteArray c2 = c.split(32*du*k,32*(du*k+dv));
+    // TODO make compress & decompress func
+}
