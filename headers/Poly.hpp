c9671cfa0968b1e80539b495c2ecf854bcf0956b
Benoit62 <65405337+Benoit62@users.noreply.github.com>
2024-04-19 13:10:44 +0200

    Aspect

--- headers/Poly.hpp
+++ headers/Poly.hpp
@@ -11,21 +11,20 @@
 #include "ByteArray.hpp"
 
 
-class Poly
-{
-private:
-    std::array<NTTCoef, n> coefficients;
+class Poly{
+    private:
+        std::array<NTTCoef, n> coefficients;
 
-public:
-    Poly();
-    template <std::size_t n>
-    Poly(const NTT& f_hat);
-    NTTCoef get(std::size_t i) const;
-    void set(std::size_t i, const NTTCoef& coef);
-    static Poly SamplePolyCBD_eta(const ByteArray& B, uint32_t eta, uint32_t q);
-    std::array<NTTCoef, n> getCoef();
-    Poly& operator=(const Poly& other);
-    Poly& operator+(const Poly& other);
+    public:
+        Poly();
+        template <std::size_t n>
+        Poly(const NTT& f_hat);
+        NTTCoef get(std::size_t i) const;
+        void set(std::size_t i, const NTTCoef& coef);
+        static Poly SamplePolyCBD_eta(const ByteArray& B, uint32_t eta, uint32_t q);
+        std::array<NTTCoef, n> getCoef();
+        Poly& operator=(const Poly& other);
+        Poly& operator+(const Poly& other);
 };
 
 #endif
